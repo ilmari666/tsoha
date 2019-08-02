03d18d0fe34ddc53ddde173a4ab415ddd5cf1fb9
tonnikala <ilmari.kuula@helsinki.fi>
2019-08-02 20:10:06 +0300

    simple navi

--- application/collections/views.py
+++ application/collections/views.py
@@ -22,10 +22,16 @@ def collection_create():
 def collections_list():
   return render_template("collections/list.html", collections = Collection.query.all())
 
-
 @app.route("/collections/publish/<collection_id>/", methods=["GET"])
 def collection_set_public(collection_id):
   collection = Collection.query.get(collection_id)
   collection.public = True
+  db.session().commit()
+  return redirect(url_for("collections_list"))
+
+@app.route("/collections/hide/<collection_id>/", methods=["GET"])
+def collection_set_private(collection_id):
+  collection = Collection.query.get(collection_id)
+  collection.public = False
   db.session().commit()
   return redirect(url_for("collections_list"))
--- application/views.py
+++ application/views.py
@@ -1,7 +1,8 @@
-from flask import render_template, request
+from flask import render_template, request, redirect
 from application import app
 
 @app.route("/")
 def index():
-  return render_template("index.html")
+  return redirect("/collections")
+#  return render_template("index.html")
 
